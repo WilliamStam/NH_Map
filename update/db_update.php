d7ac01030f3e5466efeb9961febe87d78b2d1d22
William Stam <awstam@gmail.com>
2013-06-13 10:11:20 +0200

    latest

--- update/db_update.php
+++ update/db_update.php
@@ -4,11 +4,12 @@
  * Time: 9:47 AM
  */
 $i = 1;
-
+$sql = array();
+/*
 $sql[$i++]=array(
-	"ALTER TABLE `nh_users` ADD `password` VARCHAR( 30 ) NULL DEFAULT NULL AFTER `name`;"
+	""
 );
-
+*/
  if (isset($_GET['debug'])){
 	 header("Content-Type: application/json");
 	 echo json_encode($sql);
